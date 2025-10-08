72a64f83d5aee99ff4643625026c4dc9b3340a10
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-10-08 18:35:00 +0530

    component tests

--- angular-testing-jasmine/cypress/componentTests/courseCardListComponent.spec.ts
+++ angular-testing-jasmine/cypress/componentTests/courseCardListComponent.spec.ts
@@ -2,13 +2,18 @@ import {mount} from "cypress/angular";
 import {CoursesCardListComponent} from "../../src/app/courses/courses-card-list/courses-card-list.component";
 import {CoursesModule} from "../../src/app/courses/courses.module";
 import * as mockCoursesData from './../../src/unitTestUtilities/mockData/mockCoursesData.json'
+import {CoursesService} from "../../src/app/courses/services/courses.service";
 
+class MockCourseService {
+
+}
 describe('Course Card Component', () => {
   beforeEach(() => {
     const courses = Object.assign(mockCoursesData)
     mount(CoursesCardListComponent,
       {
         imports: [CoursesModule],
+        providers: [{provide: CoursesService, useClass: MockCourseService}],
         componentProperties: {
           courses: courses.allCourses
         }
@@ -20,6 +25,8 @@ describe('Course Card Component', () => {
   });
 
   it('should open edit course dialog on click edit', () => {
-    cy.get('[data-cy=edit-course]').first()
+    cy.get('[data-cy=edit-course]').first().click()
+    cy.get('.mat-mdc-dialog-container').should('be.visible');
+    cy.get('course-dialog').should('exist')
   });
 });

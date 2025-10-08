7176e08eab298a9dc4932b8ebc6057e96fbc00bc
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-10-08 19:32:42 +0530

    component tests

--- angular-testing-jasmine/cypress/componentTests/courseCardListComponent.spec.ts
+++ angular-testing-jasmine/cypress/componentTests/courseCardListComponent.spec.ts
@@ -3,7 +3,32 @@ import {CoursesCardListComponent} from "../../src/app/courses/courses-card-list/
 import {CoursesModule} from "../../src/app/courses/courses.module";
 import * as mockCoursesData from './../../src/unitTestUtilities/mockData/mockCoursesData.json'
 import {CoursesService} from "../../src/app/courses/services/courses.service";
+import {Component, Input} from "@angular/core";
+import {RouterLink, RouterModule, RouterOutlet, Routes} from "@angular/router";
+import {Course} from "../../src/app/courses/model/course";
 
+
+@Component({
+  template: '<button data-cy="view-course-lessons" mat-raised-button color="primary" [routerLink]="[\'/courses\', course.id]">\n' +
+    '        VIEW COURSE\n' +
+    '      </button>' +
+    '<router-outlet/>',
+  imports: [RouterOutlet, RouterLink]
+})
+class CourseCardListStub {
+  @Input()
+  course: Course
+}
+
+@Component({ template: '<p>Course Works!</p>', standalone: false })
+class CourseStubComponent {}
+
+const routes: Routes = [
+  {
+    path: 'courses/:id',
+    component: CourseStubComponent
+  }
+]
 class MockCourseService {
 
 }
@@ -24,9 +49,31 @@ describe('Course Card Component', () => {
     cy.get('[data-cy=course-card]').should('have.length', 3)
   });
 
+  it('should contain course description has header', () => {
+    cy.get('[data-cy=course-card]').first().within(() => {
+      cy.get('mat-card-title').should('have.text', 'Angular Testing Course')
+    })
+  });
+
+  
+
   it('should open edit course dialog on click edit', () => {
     cy.get('[data-cy=edit-course]').first().click()
     cy.get('.mat-mdc-dialog-container').should('be.visible');
     cy.get('course-dialog').should('exist')
+  });
+
+  it('should navigate to course page on click of View course button', () => {
+    const courses = Object.assign(mockCoursesData)
+    mount(CourseCardListStub, {
+      imports: [RouterModule.forRoot(routes)],
+      declarations: [CourseStubComponent],
+      componentProperties: {
+        course: courses.allCourses[0]
+      }
+    })
+
+    cy.get('[data-cy="view-course-lessons"]').click()
+    cy.contains('Course Works!')
   });
 });
